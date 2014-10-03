047139e207eccf5d08796575bc54a1b9e3d88f5e
Anton Källbom <anton.kallbom@gmail.com>
2014-10-03 14:14:51 +0200

    uppdaterad

--- reseplanerare.c
+++ reseplanerare.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
 struct edge {
   unsigned short bus_line;
   struct node *station1;
   struct node *station2;
   unsigned short time; 
 } edge; 
-
+*/
 
 struct node {
   char * name;
-  struct connection * connections; 
-  
+  struct adjList *connections;   
 } node;
+
+struct adjList {
+  struct node *node;
+  unsigned short time;
+  struct adjList *next;
+}
 
 struct node createNode(char * name) 
 {
@@ -21,22 +26,31 @@ struct node createNode(char * name)
   return station;
 }
 
-struct edge connectNodes(unsigned short bus_line, struct node *station1, struct node *station2, unsigned short time)
+void addToAdjList (struct node *node, struct adjList *adjList, unsigned short time){
+  if (adjList->node != NULL){
+    addToAdjList (&node, &(adjList + 1), time);
+  }
+  else adjList->node = node;
+}
+/*
+void connectNodes(struct node *station1, struct node *station2, unsigned short time)
 {
-  struct edge connection = { .bus_line = bus_line, .station1 = station1, .station2 = station2, .time = time };
+  station1->adjList->node
+  struct edge connection = { .station1 = station1, .station2 = station2, .time = time };
     return connection;
 
 }
-
+*/
 
 int main ()
 {
-  struct node * nodeList = malloc(sizeof(struct node)*2);
+  struct node * nodeList = malloc(sizeof(struct node));
   struct node a = createNode("Polacksbacken");
   struct node b = createNode("Grindstugan");
   nodeList[0] = a;
   nodeList[1] = b;
 
+  addToAdjList (nodeList[1], &(nodeList->adjList), 14);
   //Lägg till edgeList
   
   struct edge ab = connectNodes(110, &a, &b, 10);
