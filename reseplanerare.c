27d71027babfbd3db4a3046f7348b3e669869858
Alex <alexander.e.lind@gmail.com>
2014-10-13 10:51:42 +0200

    fixade segfault

--- reseplanerare.c
+++ reseplanerare.c
@@ -47,8 +47,8 @@ void deleteName (struct node *Node, char *name) {
       }
     else {
       struct adjList *dp2 = *currentAdjList;
-      *currentAdjList = ((*currentAdjList)->next);
-      free(dp2);
+      currentAdjList = &((*currentAdjList)->next);
+      //free(dp2);
     }
   }
 }
@@ -58,12 +58,12 @@ void deleteNode (char *name, struct node *nodeList, unsigned short *numberOfStat
   for (int i = 0; i <= (*numberOfStations - 1); i++) {
 
     if (name == nodeList[i].name) {
-      nodeList[i] = nodeList[(*numberOfStations)-1];	
+      nodeList[i] = nodeList[(*numberOfStations)-2];	
       *numberOfStations -= 1;
     }
 
     else {
-      deleteName(&nodeList[i], name);
+      deleteName((nodeList + i), name);
     }
   } 
 }
@@ -100,37 +100,37 @@ int main (int argc, char* argv[])
       numberOfStations = atoi(argv[1]);
     }
 
-  struct node **nodeList = malloc(sizeof(struct node)*numberOfStations); //2 = numberOfStations
+  struct node *nodeList = malloc(sizeof(struct node)*numberOfStations); //2 = numberOfStations
   struct node *a = createNode("Polacksbacken");
   struct node *b = createNode("Grindstugan");
   struct node *c = createNode("Storgatan");
   struct node *d = createNode("Wallstreet");
+
   struct node *e = createNode("Levertinsgatan");
-  nodeList[0] = (a);
-  nodeList[1] = (b);
-  nodeList[2] = (c);
-  nodeList[3] = (d);
-  nodeList[4] = (e);
+  nodeList[0] = (*a);
+  nodeList[1] = (*b);
+  nodeList[2] = (*c);
+  nodeList[3] = (*d);
+  nodeList[4] = (*e);
 
   //addToAdjList ((*nodeList), (*(nodeList +1)), 14);
-  addToAdjList (a, b, 13);
-  addToAdjList (a, c, 37);
-  addToAdjList (a, d, 50);
-  addToAdjList (a, e, 42);
+  addToAdjList (nodeList, nodeList + 1, 13);
+  addToAdjList (nodeList, c, 37);
+  addToAdjList (nodeList, d, 50);
+  addToAdjList (nodeList, e, 42);
   
 
   // skriv ut test
-  printf("Stationsnamn 1: %s\nStationsnamn 2: %s\nTid mellan stationerna: %d\nTid till nästa: %d\n och: %d\n och: %d\n", a->name, b->name, a->connections->time, a->connections->next->time, a->connections->next->next->time, a->connections->next->next->next->time);
+  /* printf("Stationsnamn 1: %s\nStationsnamn 2: %s\nTid mellan stationerna: %d\nTid till nästa: %d\n och: %d\n och: %d\n", a->name, b->name, a->connections->time, a->connections->next->time, a->connections->next->next->time, a->connections->next->next->next->time);
   printf("nodeList[0]->name: %s\nnodeList[1]->name: %s\n", ((*nodeList)->name), (*(nodeList + 1))->name);
 
-
-  printConnections(*nodeList);
-
-
-  deleteNode("Grindstugan", *nodeList, &numberOfStations);
+  */
+  printConnections(nodeList);
+ 
+  deleteNode("Grindstugan", nodeList, &numberOfStations);
   //printf("nodeList[0]->name: %s\nnodeList[1]->name: %s\n", nodeList->name, (nodeList +1)->name);
 
-  printConnections(*nodeList);
+  printConnections(nodeList);
   
   free(a);
   free(b);
